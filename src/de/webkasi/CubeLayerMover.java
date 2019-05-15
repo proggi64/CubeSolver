4dee50286c1abb8e0fc2844c4966d2e3c122d3ba
Karsten <karsten@webkasi.de>
2019-05-15 20:10:14 +0200

    Start Rubik's Solver: Datenstruktur und Konsolenausgabe

--- src/de/webkasi/CubeLayerMover.java
+++ src/de/webkasi/CubeLayerMover.java
@@ -2,9 +2,26 @@ package de.webkasi;
 
 class CubeLayerMover {
     private Cube _cube;
+    private CubeSide[][] topToSides;
 
     CubeLayerMover(Cube cube) {
         _cube = cube;
+        CubeSide[] sides = _cube.getSides();
+        final int White     = CubeColor.White.ordinal();
+        final int Orange    = CubeColor.Orange.ordinal();
+        final int Green     = CubeColor.Green.ordinal();
+        final int Red       = CubeColor.Red.ordinal();
+        final int Blue      = CubeColor.Blue.ordinal();
+        final int Yellow    = CubeColor.Yellow.ordinal();
+
+        topToSides = new CubeSide[][] {
+                { sides[Orange], sides[Green], sides[Red], sides[Blue], sides[Yellow] },    // White
+                { sides[White], sides[Blue], sides[Yellow], sides[Green], sides[Red] },     // Orange
+                { sides[White], sides[Orange], sides[Yellow], sides[Red], sides[Blue] },    // Green
+                { sides[White], sides[Green], sides[Yellow], sides[Blue], sides[Orange] },  // Red
+                { sides[White], sides[Red], sides[Yellow], sides[Orange], sides[Green] },   // Blue
+                { sides[Orange], sides[Blue], sides[Red], sides[Green], sides[White] }      // Yellow
+        };
     }
 
     void move(CubeLayerMoveDirection direction, CubeColor side, int startLayer, int countOfLayers) {
@@ -16,7 +33,27 @@ class CubeLayerMover {
     }
 
     private void shiftSideLayerLeft(CubeColor side, int layer) {
+        CubeSide[] sidesToShift = topToSides[side.ordinal()];
+        CubeSide[] originals = new CubeSide[] {
+                new CubeSide(sidesToShift[0]),
+                new CubeSide(sidesToShift[1]),
+                new CubeSide(sidesToShift[2]),
+                new CubeSide(sidesToShift[3]),
+                new CubeSide(sidesToShift[4])
+        };
+        int maxIndex = _cube.getDimension();
 
+        for (int sideIndex = 0; sideIndex <= 3; sideIndex++) {
+            int targetSideIndex = (sideIndex + 1) % 3;
+            CubeSide targetSide = sidesToShift[targetSideIndex];
+            CubeColor[] targetColorRow = targetSide.getFields()[layer];
+            final CubeSide sourceSide = originals[sideIndex];
+            final CubeColor[] sourceColorRow = targetSide.getFields()[layer];
+
+            for (int fieldIndex = 0; fieldIndex < maxIndex; fieldIndex++) {
+                
+            }
+        }
     }
 
     private void rotateSideRight(CubeColor side) {
